63a98f9c249275572dec832fdeab2842fe711790
mairisdev <mairisdigital@icloud.com>
2025-07-31 17:13:09 +0300

    Commit 1-2

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <div>
+          <h1 className="text-4xl font-bold text-red-500">Welcome to Our Site</h1>
+        </div>
+      </main>
+      <footer className="text-center text-sm text-gray-500 row-start-3">
+        © 2025 Martas Mēbeles, All rights reserved.
+      </footer>
+    </div>
+  );
+}
